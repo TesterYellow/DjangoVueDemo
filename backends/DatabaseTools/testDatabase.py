5624f34f243f22fd74fc405274e0b35d8bc9e93b
hsh0730 <tester_yellow@126.com>
2023-03-06 22:55:17 +0800

    models.py代码重写【直播录制】& testDatabase文件内容

--- backends/DatabaseTools/testDatabase.py
+++ backends/DatabaseTools/testDatabase.py
@@ -5,8 +5,64 @@
 """
 import logging
 
+import pymysql
+
+from Code.TesterYellow.backends.models import DemoTestDatabase
+
+'''
+本章笔记
+1，logger = logging.getLogger("log") 创建日志对象 【详细介绍：https://blog.csdn.net/liming89/article/details/109609557】
+2，引入models中定义的数据库表，获取数据库连接信息；
+3，与connectionDB.py区别：可以根据数据库中查询到的连接信息查询指定数据库内容；
+4，pymysql：python操作mysql数据库的第三方工具库
+'''
+
 logger = logging.getLogger("log")
 
 
 def getSql(id, sql):
-	pass
+	try:
+		# 根据数据库id查询指定数据库信息，并返回数据对象
+		database = DemoTestDatabase.objects.filter(id=id)
+		host = database.values("host")[0]["host"]
+		username = database.values("username")[0]["username"]
+		password = database.values("password")[0]["password"]
+		port = database.values("port")[0]["port"]
+		connect = pymysql.Connect(host=str(host), user=str(username), passwd=str(password), port=int(port),
+								  charset="utf8")
+		# 获取游标
+		cursor = connect.cursor()
+		cursor.execute(sql)
+		rawData = cursor.fetchall()
+		# 获取表的字段名称
+		col_names = [desc[0] for desc in cursor.description]
+		result = []
+		for row in rawData:
+			objectDict = {}
+			# 遍历每一行数据并放到objectDict中
+			for index, value in enumerate(row):
+				objectDict[col_names[index]] = value
+			result.append(objectDict)
+		return result
+	except Exception as e:
+		logger.error(e)
+	finally:
+		cursor.close()
+		connect.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- backends/models.py
+++ backends/models.py
@@ -4,17 +4,36 @@
 @File ：models.py
 """
 
-# 项目模块
 from django.db import models
+
+'''
+本章笔记：
+
+一、字段类型
+1，AutoField：映射到数据库为int类型，具有自增的特性，需要设置属性primary_key=True；
+2，TextField：文本类型
+3，DateTImeField：时间类型；
+4，CharField：字符串类型
+
+二、属性
+1，primary_key: 是否为主键；
+2，autoincrement：自增；
+3，null：是否允许为空；
+4，db_index：建立索引：
+5，default：设置默认值：
+6，auto_now_add：添加时间为当前时间；
+7，auto_now：修改时间为当前时间；
+8，max_length：限制字段的最大长度；
+'''
 
 
 # 运行环境表
 class DemoTestOperatingEnvironment(models.Model):
-	oe_id = models.AutoField(primary_key=True, null=False, db_index=True)  # id
-	oe_text = models.TextField(default="")  # 环境内容
-	create_time = models.DateTimeField(auto_now_add=True)  # 创建时间
-	update_time = models.DateTimeField(auto_now=True)  # 修改时间
-	remark = models.CharField(max_length=60, default='')  # 备注
+	oe_id = models.AutoField(primary_key=True, null=False, db_index=True)  # 运行环境id
+	oe_text = models.TextField(default="")  # 运行环境内容
+	create_time = models.DateTimeField(auto_now_add=True)  # 当前数据的创建时间
+	update_time = models.DateTimeField(auto_now=True)  # 当前数据的修改时间
+	remark = models.CharField(max_length=60, default='')  # 备注说明
 
 
 # 项目表
@@ -27,7 +46,7 @@ class DemoTestProject(models.Model):
 	other_configure = models.TextField(default='')  # 微服务运行地址2
 	create_time = models.DateTimeField(auto_now_add=True)  # 当前数据的创建时间
 	update_time = models.DateTimeField(auto_now=True)  # 当前数据的修改时间
-	remark = models.CharField(max_length=60, default='')  # 备注说明
+	remark = models.CharField(max_length=60, default="")  # 备注说明
 
 
 # 分组表
@@ -35,131 +54,134 @@ class DemoTestGrouping(models.Model):
 	group_id = models.AutoField(primary_key=True, null=False, db_index=True)  # 分组id
 	group_name = models.TextField()  # 分组名称
 	group_pj_id = models.TextField()  # 关联项目id
-	create_time = models.DateTimeField(auto_now_add=True)  # 创建时间
-	update_time = models.DateTimeField(auto_now=True)  # 修改时间
-	remark = models.CharField(max_length=60, default='')  # 备注说明
+	create_time = models.DateTimeField(auto_now_add=True)  # 当前数据的创建时间
+	update_time = models.DateTimeField(auto_now=True)  # 当前数据的修改时间
+	remark = models.CharField(max_length=60, default="")  # 备注说明
 
 
 # 接口表
 class DemoTestRequest(models.Model):
 	request_id = models.AutoField(primary_key=True, null=False, db_index=True)  # 接口id
 	request_name = models.TextField()  # 接口名称
-	request_group_id = models.TextField()  # 分组id
+	request_group_id = models.TextField()  # 关联分组id
 	request_address = models.TextField()  # 接口地址
-	request_mode = models.TextField(0)  # 请求接口方式
-	create_time = models.DateTimeField(auto_now_add=True)  # 创建时间
-	update_time = models.DateTimeField(auto_now=True)  # 修改时间
-	remark = models.CharField(max_length=60, default='')  # 备注说明
+	request_mode = models.TextField()  # 接口请求方式
+	create_time = models.DateTimeField(auto_now_add=True)  # 当前数据的创建时间
+	update_time = models.DateTimeField(auto_now=True)  # 当前数据的修改时间
+	remark = models.CharField(max_length=60, default="")  # 备注
 
 
 # 测试用例表
-class DemoRequestTestCase(models.Model):
+class DemoTestRequestTestCase(models.Model):
 	case_id = models.AutoField(primary_key=True, null=False, db_index=True)  # 用例id
 	case_name = models.TextField()  # 用例名称
 	case_request_id = models.TextField()  # 关联接口id
-	request_header_param = models.TextField(default='')  # header 请求头信息
-	headerData = models.TextField(default='')  # header动态取值内容
-	request_data_content = models.TextField(default='')  # 请求参数
-	request_other_type = models.TextField(default='')  # 是否使用关联接口
-	request_relation_content = models.TextField(default='')  # 关联接口内容 1：存入接口地址，入参数据和提取数据字段
-	request_response = models.TextField(default='')  # 响应结果
-	request_code = models.TextField(default='')  # 断言结果
-	request_result = models.TextField(default='')  # 入参动态 断言内容
-	request_result_Assertion_content = models.TextField(default='')  # 结果断言内容
-	create_time = models.DateTimeField(auto_now_add=True)  # 创建时间
-	update_time = models.DateTimeField(auto_now=True)  # 修改时间
-	other_case_id = models.TextField(default='')  # 关联其他用例id
-	other_case_id_conclusion_value = models.TextField(default='')  # 关联其他取值
-	data_type = models.TextField(default='')  # 接口请求类型： json|type
+	request_header_param = models.TextField(default="")  # 请求头
+	header_data = models.TextField(default="")  # 请求头动态取值内容
+	request_data_content = models.TextField(default="")  # 请求接口参数
+	request_other_type = models.TextField(default="")  # 是否使用关联接口
+	request_relation_content = models.TextField(default="")  # 关联接口内容 1.存入接口地址，入参数据和提取数据
+	request_response = models.TextField(default="")  # 响应内容
+	request_code = models.TextField(default="")  # 断言结果
+	request_result = models.TextField(default="")  # 入参动态断言内容
+	request_result_Assertion_content = models.TextField(default="")  # 结果断言内容
+	create_time = models.DateTimeField(auto_now_add=True)  # 当前数据的创建时间
+	update_time = models.DateTimeField(auto_now=True)  # 当前数据的修改时间
+	other_case_id = models.TextField(default="")  # 关联其他用例id
+	other_case_id_conclusion_value = models.TextField(default="")  # 关联其他取值
+	data_type = models.TextField(default="")  # 接口请求参数 JSON|type
 	sql_content = models.TextField(default="")  # 前后置接口内容
-	remark = models.CharField(max_length=60, default='')  # 备注说明
+	remark = models.CharField(max_length=60, default="")  # 备注说明
 
 
-# 存储公共接口表
+# 存储公共接口
 class DemoTestPublicInterface(models.Model):
-	request_id = models.AutoField(primary_key=True, null=False, db_index=True)
-	url = models.TextField()  # 请求接口的地址
-	method = models.CharField(max_length=10)  # 接口请求方式：post get
-	request_data_type = models.TextField(default='')  # 接口请求类型
-	request_header_param = models.TextField(default='')  # 请求头
-	request_data_content = models.TextField(default='')  # 请求参数
-	request_value = models.TextField(default='')  # 接口动态取值
-	create_time = models.DateTimeField(auto_now_add=True)  # 创建时间
-	update_time = models.DateTimeField(auto_now=True)  # 修改时间爱你
-	remark = models.CharField(max_length=60, default='')  # 备注说明
+	request_id = models.AutoField(primary_key=True, null=False, db_index=True)  # primary_key：主键，自动增量；null：是否可以为空
+	url = models.TextField()  # 接口请求地址
+	method = models.CharField(max_length=10)  # 接口请求方式 post get
+	request_data_type = models.TextField(default="")  # 接口请求类型 JSON|type
+	request_header_param = models.TextField(default="")  # 头部信息
+	request_data_content = models.TextField(default="")  # 请求接口参数
+	request_value = models.TextField(default="")  # 接口动态取值
+	create_time = models.DateTimeField(auto_now_add=True)  # 当前数据的创建时间
+	update_time = models.DateTimeField(auto_now=True)  # 当前数据的修改时间
+	remark = models.CharField(max_length=60, default="")  # 备注说明
 
 
-# 计划和结果报表主表
+# 计划和结果报表-主表
 class DemoResponseContentReport(models.Model):
-	plan_id = models.AutoField(primary_key=True, null=False, db_index=True, max_length=200)
-	plan_name = models.TextField(default='')  # 计划名称
-	store_test_case = models.TextField(default='')  # 存入的测试用例
-	execution_state = models.TextField(default='')  # 执行状态
-	execution_progress = models.TextField(default='')  # 执行进度
-	success_case = models.TextField(default='')  # 成功用例
-	fail_case = models.TextField(default='')  # 失败用例
-	skip_case = models.TextField(default='')  # 跳过用例
-	case_total = models.TextField(default='')  # 执行用例总数
-	create_time = models.DateTimeField(auto_now_add=True)
-	update_time = models.DateTimeField(auto_now=True)
+	plan_id = models.AutoField(primary_key=True, null=False, db_index=True, max_length=200)  # 主键 计划id
+	plan_name = models.TextField(default="")  # 计划名称
+	store_test_case = models.TextField(default="")  # 存入的测试用例
+	execution_state = models.TextField(default="")  # 执行状态
+	execution_progress = models.TextField(default="")  # 执行进度
+	success_case = models.TextField(default="")  # 成功用例
+	fail_case = models.TextField(default="")  # 失败用例
+	skip_case = models.TextField(default="")  # 跳过用例
+	case_total = models.TextField(default="")  # 执行用例总数
+	create_time = models.DateTimeField(auto_now_add=True)  # 当前数据的创建时间
+	update_time = models.DateTimeField(auto_now=True)  # 当前数据的修改时间
 	timing_open_close = models.TextField(default='')  # 是否开启定时任务
-	timing_task = models.TextField(default='')  # 定时任务
-	remark = models.CharField(max_length=60, default='')
+	timing_task = models.TextField(default="")  # 定时任务
+	remark = models.CharField(max_length=60, default="")  # 备注
 
 
-# 响应结果表
-class DemoResponseReportResult(models.Model):
-	id = models.AutoField(primary_key=True, null=True, db_index=True)
-	plan_id = models.AutoField(primary_key=True, null=False, db_index=True, max_length=200)
-	plan_name = models.TextField(default='')  # 计划名称
-	success_case = models.TextField(default='')  # 成功用例
-	fail_case = models.TextField(default='')  # 失败用例
-	skip_case = models.TextField(default='')  # 跳过用例
-	case_total = models.TextField(default='')  # 执行用例总数
-	create_time = models.DateTimeField(auto_now_add=True)
-	update_time = models.DateTimeField(auto_now=True)
-	timing_open_close = models.TextField(default='')  # 是否开启定时任务
-	remark = models.CharField(max_length=60, default='')
+# 结果报表
+class DemoResponseResult(models.Model):
+	id = models.AutoField(primary_key=True, null=False, db_index=True)  # 主键
+	plan_id = models.CharField(null=False, db_index=True, max_length=200)  # 任务计划id
+	plan_name = models.TextField(default="")  # 计划名称
+	success_case = models.TextField(default="")  # 成功用例
+	fail_case = models.TextField(default="")  # 失败用例
+	skip_case = models.TextField(default="")  # 跳过用例
+	case_total = models.TextField(default="")  # 执行用例总数
+	create_time = models.DateTimeField(auto_now_add=True)  # 当前数据的创建时间
+	update_time = models.DateTimeField(auto_now=True)  # 当前数据的修改时间
+	timing_open_close = models.CharField(default="")  # 是够开启定时任务
+	remark = models.CharField(max_length=60, default="")  # 备注说明
 
 
-# 链接测试数据库
+# 测试数据库表
 class DemoTestDatabase(models.Model):
-	id = models.AutoField(primary_key=True, null=False, db_index=True)
-	host = models.CharField(max_length=60)
-	username = models.CharField(max_length=60)
-	password = models.CharField(max_length=60)
-	port = models.CharField(max_length=60)
-	remark = models.CharField(max_length=60, default='')
+	id = models.AutoField(primary_key=True, null=False, db_index=True)  # 主键id
+	host = models.CharField(max_length=60)  # 数据库连接host
+	username = models.CharField(max_length=60)  # 连接用户名
+	password = models.CharField(max_length=60)  # 连接密码
+	port = models.CharField(max_length=60, default=3306)  # 连接端口&默认值
+	remark = models.CharField(max_length=60, default="")  # 备注说明
 
 
-# 测试邮箱
+# 邮箱表
 class DemoTestEmail(models.Model):
 	id = models.AutoField(primary_key=True, null=False)
-	host = models.TextField(default='')  # SMTP服务器
-	port = models.TextField(default='')  # 端口号
-	user = models.TextField(default='')  # 用户名
-	password = models.TextField(default='')  # 密码
-	sender = models.TextField(default='')  # 发件人
-	receivers = models.TextField(default='')  # 收件人
-	remark = models.TextField(default='')  # 备注
+	host = models.TextField(default="")  # SMTP服务器: smtp.163.com
+	port = models.TextField(default="")  # 端口号
+	user = models.TextField(default="")  # 邮箱用户名
+	password = models.TextField(default="")  # 邮箱授权密码，非登录密码
+	sender = models.TextField(default="")  # 发件人
+	receivers = models.TextField(default="")  # 收件人
+	remark = models.TextField(default="")  # 备注说明
 
 
 # 用户表
 class DemoUser(models.Model):
 	user_type_choices = ((1, "一级权限"), (2, "二级权限"), (3, "三级权限"))
-	user_id = models.AutoField(primary_key=True, null=False, db_index=True)
-	user_name = models.CharField(max_length=60, db_index=True)  # 用户名
-	user_account = models.CharField(max_length=60)  # 用户登录账号
-	user_password = models.CharField(max_length=60)  # 密码
-	create_time = models.DateTimeField(auto_now_add=True)
-	update_time = models.DateTimeField(auto_now=True)
-	user_type = models.IntegerField(choices=user_type_choices, default=1)  # 用户类型，默认1
-	remark = models.CharField(max_length=60, default='')
-	token = models.CharField(max_length=60, null=True)
+	# username = models.CharField(max_length=50, unique=True) # 用户名，且需要保证唯一
+	# password = models.CharField(max_length=100) # 登录密码
+	user_id = models.AutoField(primary_key=60, null=False, db_index=True) # 主键：用户id
+	user_name = models.CharField(max_length=60, db_index=True)  # 用户名称
+	user_account = models.CharField(max_length=100)  # 登录账号
+	user_password = models.CharField(max_length=100)  # 用户登录密码
+	create_time = models.DateTimeField(auto_now_add=True)  # 创建时间
+	update_time = models.DateTimeField(auto_now=True)  # 修改时间
+	user_type = models.IntegerField(choices=user_type_choices, default=1)  # 用户类型， 默认1
+	remark = models.CharField(max_length=60, default="")  # 备注说明
+	token = models.CharField(max_length=60, null=True)  # 登录token值
 
 
-class DemoTestHeads(models.Model):
+# 请求头信息表
+class DemoTEstHeaders(models.Model):
 	id = models.AutoField(primary_key=True, null=False)
-	tpi_id = models.TextField(default='') # 关联公共接口id
-	headerName = models.TextField() # 头部名称
-	headerValues = models.TextField() # 头部值
+	tpi_id = models.TextField(default="")  # 关联公共接口id
+	headers_name = models.TextField()  # 头部名称
+	headers_values = models.TextField()  # 头部值
